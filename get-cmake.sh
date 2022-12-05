771574359ba5a66bdc28cd3f742b46bd2dc05583
Ivan Dyachkov <dev@dyachkov.org>
2022-12-05 21:27:48 +0100

    chore: add amazonlinux2 bulder platform

--- get-cmake.sh
+++ get-cmake.sh
@@ -3,26 +3,27 @@
 set -eu
 
 VSN=${2:-"3.19.2"}
+ARCH=$(arch)
 
 if [ -d '/usr/local/openssl' ]; then
     export OPENSSL_ROOT_DIR='/usr/local/openssl'
     echo "OPENSSL_ROOT_DIR=$OPENSSL_ROOT_DIR"
 fi
-if [ "$1" = 'build' ]; then
+if [ "${1:-}" = 'build' ]; then
     curl --silent --show-error -kfL -o cmake.tar.gz "https://github.com/Kitware/CMake/releases/download/v${VSN}/cmake-${VSN}.tar.gz"
     tar -zxf cmake.tar.gz
     rm -f cmake.tar.gz
     cd "cmake-${VSN}/"
 
-    ./bootstrap --parallel=8
-    make -j8
+    ./bootstrap --parallel=$(nproc)
+    make -j$(nproc)
     make install
     cd ..
     rm -rf "cmake-${VSN}/"
     cmake --version
 else
-    curl --silent --show-error -kfL -o cmake.tar.gz "https://github.com/Kitware/CMake/releases/download/v${VSN}/cmake-${VSN}-Linux-x86_64.tar.gz"
+    curl --silent --show-error -kfL -o cmake.tar.gz "https://github.com/Kitware/CMake/releases/download/v${VSN}/cmake-${VSN}-Linux-$ARCH.tar.gz"
     tar -zxf cmake.tar.gz
     rm -f cmake.tar.gz
-    mv "cmake-${VSN}-Linux-x86_64/" /cmake
+    mv "cmake-${VSN}-Linux-$ARCH/" /cmake
 fi

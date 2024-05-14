a73935d068fc2eecaf66ff0ed1119707a19f6a40
zhongwencool <zhongwencool@gmail.com>
2024-05-14 11:45:46 +0800

    feat: install zsh on unbuntu for developing

---
+++ get-zsh.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+URL="https://github.com/deluan/zsh-in-docker/releases/download/v1.1.5/zsh-in-docker.sh"
+
+cd /
+curl --silent --show-error -fkL "$URL" -o "zsh-in-docker.sh"
+chmod +x zsh-in-docker.sh
+
+## build
+
+ ./zsh-in-docker.sh \
+    -t https://github.com/denysdovhan/spaceship-prompt \
+    -a 'SPACESHIP_PROMPT_ADD_NEWLINE="false"' \
+    -a 'SPACESHIP_PROMPT_SEPARATE_LINE="false"' \
+    -p git \
+    -p git-fast
+    -p https://github.com/zsh-users/zsh-autosuggestions \
+    -p https://github.com/zsh-users/zsh-completions \
+    -p https://github.com/zsh-users/zsh-history-substring-search \
+    -p https://github.com/zsh-users/zsh-syntax-highlighting \
+    -p 'history-substring-search' \
+    -a 'bindkey "\$terminfo[kcuu1]" history-substring-search-up' \
+    -a 'bindkey "\$terminfo[kcud1]" history-substring-search-down'
+
+
+## cleanup
+cd /
+rm -rf zsh-in-docker.sh
+

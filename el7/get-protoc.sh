c534fd97efefe354cb808159078d93f1350c2d93
Thales Macedo Garitezi <16166434+thalesmg@users.noreply.github.com>
2025-12-17 10:49:07 -0300

    chore(el7): add script to get protobuf compiler

---
+++ el7/get-protoc.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cd /tmp
+
+PB_REL="https://github.com/protocolbuffers/protobuf/releases"
+VSN="33.1"
+
+case $(uname -m) in
+  x86_64)
+    ARCH="x86_64"
+    ;;
+  *)
+    ARCH="aarch_64"
+    ;;
+esac
+
+curl -LO "$PB_REL/download/v$VSN/protoc-$VSN-linux-$ARCH.zip"
+# zip contains `bin/protoc`
+unzip "protoc-$VSN-linux-$ARCH.zip" -d /usr/local/

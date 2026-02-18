ec7a9870b0ba823be02e11bb7acace50a09243f7
Ilia Averianov <av@rubybox.dev>
2026-02-18 14:10:12 +0200

    fix: significantly reduce rust toolchain size

--- get-rust.sh
+++ get-rust.sh
@@ -5,6 +5,7 @@ set -eu
 RUST_VERSION="${1:-1.88.0}"
 
 if [ -n "${RUST_VERSION}" ]; then
-  rustup toolchain install ${RUST_VERSION}
+  rustup toolchain install --profile minimal ${RUST_VERSION}
   rustup default ${RUST_VERSION}
+
 fi
